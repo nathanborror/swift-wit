b85da0d7151436cf033fab553e79546fc0a87bf2
Nathan Borror <nathan@nathanborror.com>
2025-07-30 21:58:33 -0700

    Test returning dicts and decoding sections

--- Tests/WitTests/ConfigTests.swift
+++ Tests/WitTests/ConfigTests.swift
@@ -17,6 +17,11 @@ final class ConfigTests {
         let config = ConfigDecoder().decode(input)
         #expect(config["core.version"] == "1.0")
         #expect(config["user.name"] == "Test User")
+
+        guard case .dictionary(let dict) = config[section: "core"] else {
+            fatalError("failed to extract dictionary")
+        }
+        #expect(dict == ["version": "1.0"])
     }
 
     @Test("Encoding")
@@ -31,6 +36,33 @@ final class ConfigTests {
         let config: [String: Section] = [
             "core": .dictionary(["version": "1.0"]),
             "user": .dictionary(["name": "Test User"]),
+        ]
+
+        let encoded = ConfigEncoder().encode(config)
+        #expect(encoded == expecting)
+    }
+
+    @Test("Decoding keyed sections")
+    func decodingKeyedSections() {
+        let input = """
+            [remote "local"]
+                host = http://localhost:8080
+            """
+
+        let config = ConfigDecoder().decode(input)
+        print(config)
+        #expect(config["remote:local.host"] == "http://localhost:8080")
+    }
+
+    @Test("Encoding keyed sections")
+    func encodingKeyedSections() {
+        let expecting = """
+            [remote "local"]
+                host = http://localhost:8080
+            """
+
+        let config: [String: Section] = [
+            "remote:local": .dictionary(["host": "http://localhost:8080"]),
         ]
 
         let encoded = ConfigEncoder().encode(config)
