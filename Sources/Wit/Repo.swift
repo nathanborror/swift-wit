aeb50ddb41fcc00207aa8ff43d932a7a775d0e34
Nathan Borror <nathan@nathanborror.com>
2026-02-03 12:16:46 -0800

    Override base URL if you wish

--- Sources/Wit/Repo.swift
+++ Sources/Wit/Repo.swift
@@ -35,8 +35,8 @@ public actor Repo {
     let privateKey: Remote.PrivateKey?
     let ignores: [String]
 
-    public init(path: String, objectsPath: String? = nil, privateKey: Remote.PrivateKey? = nil, ignores: [String] = [".DS_Store"]) {
-        self.localURL = URL.documentsDirectory.appending(path: path)
+    public init(path: String, objectsPath: String? = nil, privateKey: Remote.PrivateKey? = nil, ignores: [String] = [".DS_Store"], baseURL: URL = .documentsDirectory) {
+        self.localURL = baseURL.appending(path: path)
         self.privateKey = privateKey
         self.local = RemoteDisk(baseURL: localURL)
         self.objects = Objects(
